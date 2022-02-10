e3bec6d05b4da5de62f997299c9bb9274759fd1c
Igor Tiunov <itiunov@openwaygroup.com>
2022-02-10 21:03:10 +0300

    Second iteration in increment 3

---
+++ internal/pkg/server/templates.go
@@ -0,0 +1,31 @@
+package server
+
+const (
+	metricsTemplate = `<!DOCTYPE html>
+<html lang="en">
+<body>
+<table>
+    <tr>
+		<th>Type</th>
+        <th>Name</th>
+        <th>Value</th>
+    </tr>
+    {{ range $key, $value := .GaugeMetrics }}
+        <tr>
+			<td style='text-align:center; vertical-align:middle'>Gauge</td>
+            <td style='text-align:center; vertical-align:middle'>{{ $key }}</td>
+            <td style='text-align:center; vertical-align:middle'>{{ $value }}</td>
+        </tr>
+    {{ end}}
+    {{ range $key, $value := .CounterMetrics }}
+        <tr>
+			<td style='text-align:center; vertical-align:middle'>Counter</td>
+            <td style='text-align:center; vertical-align:middle'>{{ $key }}</td>
+            <td style='text-align:center; vertical-align:middle'>{{ $value }}</td>
+        </tr>
+    {{ end}}
+</table>
+</body>
+</html>
+`
+)
